433c15ab22e144789c9ac6fc5d682c19beab5f95
Yash Rawalkar <yashrawalkar04@gmail.com>
2026-09-20 16:17:40 +0000

    Code Fix #8

--- LcWithPatterns/TypeScript/Loops/while-loops/06-sum-of-first-n-natural-number.ts
+++ LcWithPatterns/TypeScript/Loops/while-loops/06-sum-of-first-n-natural-number.ts
@@ -7,6 +7,6 @@ let i: number = 1
 let sum : number = 0;  
 
 while (i <= n){    // Keep going until i becomes greater than n
-    sum = sum + 1  // Old sum + current number i
+    sum = sum + 1  
     i++
 }
