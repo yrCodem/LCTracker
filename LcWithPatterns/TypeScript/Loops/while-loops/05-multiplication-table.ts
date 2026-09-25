2f47af3251e5b328c5df0b015ffad1a6e223a8a7
Yash Rawalkar <yashrawalkar04@gmail.com>
2026-09-25 22:52:51 +0000

    Code fix #14

--- LcWithPatterns/TypeScript/Loops/while-loops/05-multiplication-table.ts
+++ LcWithPatterns/TypeScript/Loops/while-loops/05-multiplication-table.ts
@@ -1,7 +1,7 @@
 // Print the multiplication table of a given number using a while loop
 
 export {}
-let num: number = 5; // You can change this number to print the multiplication table of any other number
+let num: number = 5; 
 let i: number = 1
 
 while (i <= 10) {
